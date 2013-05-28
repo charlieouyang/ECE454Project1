91f14b738c7db3776a9ab91c4b6c0f83ac85ebff
Justen Pinto <justen.pinto@gmail.com>
2013-05-27 23:46:09 -0400

    static and renamed

--- ECE454Project1/src/justen/DirectoryHelper.java
+++ ECE454Project1/src/justen/DirectoryHelper.java
@@ -1,45 +1,36 @@
 package justen;
 import java.io.File;
 
-public class MiscFunctions {
+public class DirectoryHelper {
 	
-	public boolean createDirectory(String name)
+	public static boolean createDirectory(String name)
 	{
 		File dir = new File(name);
 		if (dir.isDirectory())
-		{
 			deleteFolder(dir);
-			return true;
-		}
 		return dir.mkdir();
 	}
 	
-	public boolean createCompletedDirectory(String name)
+	public static boolean createCompletedDirectory(String name)
 	{
 		File dir = new File(name + "\\completed");
 		if (dir.isDirectory())
-		{
 			deleteFolder(dir);
-			return true;
-		}
 		return dir.mkdir();
 	}
 	
-	public boolean createChunkDirectory(String name)
+	public static boolean createChunkDirectory(String name)
 	{
 		File dir = new File(name + "\\chunks");
 		if (dir.isDirectory())
-		{
 			deleteFolder(dir);
-			return true;
-		}
 		return dir.mkdir();
 	}
 	
 	/*
 	 * Recursively deletes all files in directory
 	 * */
-	private void deleteFolder(File folder)
+	private static void deleteFolder(File folder)
 	{
 		File[] files = folder.listFiles();
 	    if(files!=null) { //some JVMs return null for empty dirs
