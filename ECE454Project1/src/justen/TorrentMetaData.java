d16b4e89a1ba714e25ed06d21524ab9b49e01b7f
Justen Pinto <justen.pinto@gmail.com>
2013-06-01 22:12:22 -0400

    waddup

--- ECE454Project1/src/justen/TorrentMetaData.java
+++ ECE454Project1/src/justen/TorrentMetaData.java
@@ -5,8 +5,16 @@ public class TorrentMetaData {
 	private String fileName;
 	private int numChunks;
 	
-	public TorrentMetaData(TorrentFile tFile) {
-		fileName = tFile.getFileName();
-		numChunks = (int)tFile.getNumberOfChunks();
+	public TorrentMetaData(String name, int chunks) {
+		fileName = name;
+		numChunks = chunks;
+	}
+	
+	public String getFileName() {
+		return fileName;
+	}
+	
+	public int getNumberOfChunks() {
+		return numChunks;
 	}
 }
