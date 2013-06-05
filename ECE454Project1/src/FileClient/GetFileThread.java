c4aa33a6dba18605267e08b856cb604b08dc347c
Charlie Ou Yang <charlieouyang@hotmail.com>
2013-06-04 21:04:24 -0400

    I hope this shit works!!!!

--- ECE454Project1/src/FileClient/GetFileThread.java
+++ ECE454Project1/src/FileClient/GetFileThread.java
@@ -6,6 +6,7 @@ import java.util.Iterator;
 import java.util.Map;
 
 import justen.ConcurrencyManager;
+import justen.InflightChunkRequestManager;
 import justen.Status;
 import justen.TorrentMetaData;
 import Data.PropertiesOfPeer;
@@ -14,47 +15,65 @@ public class GetFileThread extends Thread {
 	String fileName;
 	TorrentMetaData fileMettaData;
 	Status peerStatus;
-	boolean fileTransferComplete;
 	ConcurrencyManager concurManager;
+	InflightChunkRequestManager icrm;
+	Hashtable<String, Status> listOfOtherPeersStatus;
 	
 	public GetFileThread(String fileName, TorrentMetaData fileMettaData) {
 		this.fileName = fileName;
 		this.fileMettaData = fileMettaData;
 		this.peerStatus = PropertiesOfPeer.getCurrentPeerStatus();
 		this.concurManager = PropertiesOfPeer.peerConcurrencyManager;
-		this.fileTransferComplete = false;
+		this.icrm = new InflightChunkRequestManager(fileMettaData.getNumberOfChunks());
+		this.listOfOtherPeersStatus = PropertiesOfPeer.listOfOtherPeersStatus;
 	}
 
 	@Override
 	public void run() {
 		try {
-			while (!fileTransferComplete) {
+			Hashtable<String, Integer[]> fileNameChunkReplicationMap = peerStatus.fileNameChunkReplicationMap;
+			Integer[] fileReplicationArray = (Integer[]) fileNameChunkReplicationMap.get(fileName);
+			this.listOfOtherPeersStatus = PropertiesOfPeer.listOfOtherPeersStatus;
+			
+			while (true) {
 				// Keep running this until file is complete
 				
+				//Check local chunks, and pass whatever he said to himself
+				//Determine which chunk index am I going to check
+				//Ask Pinto if we need this chunk
+				//Ask Pinto if this chunk is already in flight
+				//Tell Pinto chunk request sent right after
 				
-				
-				// Manage all of the chunk requests and chunk to host map
-				Hashtable<Integer, String> chunkToHostMapping = new Hashtable<Integer, String>();
-				int numberOfChunks = fileMettaData.getNumberOfChunks();
+				ArrayList<Integer> previousIndexes = new ArrayList<Integer>();
+				int j = 0;
+				while (true) {
 
-				/*
-				 * for (int i = 0; i < numberOfChunks; i++) { //Get the chunk#
-				 * to host mapping for chunk requests chunkToHostMapping.put(i,
-				 * getChunkToHostMapping(fileName, i)); }
-				 */
+					int minValue = Integer.MAX_VALUE;
+					int minIndex = 0;
+					for (int i = 0; i < fileReplicationArray.length; i++) {
+						if (fileReplicationArray[i] < minValue && !previousIndexes.contains(i)) {
+							minValue = fileReplicationArray[i];
+							minIndex = i;
+						}
+					}
+					previousIndexes.add(minIndex);
+					//Call Pinto's function with minIndex
+					boolean shouldWeGetThisChunk = icrm.chunkNeeded(minIndex);
+					
+					if (shouldWeGetThisChunk){
+						if (!icrm.isChunkInflight(minIndex)){
+							//Find out which host has this chunk
+							String hostNameToGetChunk = getHostNameForChunk(fileName, minIndex);
 
-				// Get the chunk# to host mapping for chunk requests
-				chunkToHostMapping.put(i, getChunkToHostMapping(fileName));
-
-				// Start sending out chunk request
-				Iterator<Map.Entry<Integer, String>> mappingIterator = chunkToHostMapping
-						.entrySet().iterator();
-				while (mappingIterator.hasNext()) {
-					Map.Entry<Integer, String> entry = mappingIterator.next();
-
-					RequestForChunk requestForChunkThread = new RequestForChunk(
-							fileName, entry.getValue(), entry.getKey());
-					requestForChunkThread.start();
+							icrm.noteChunkRequest(minIndex);
+							//Send chunk request
+							RequestForChunk requestForChunkThread = new RequestForChunk(fileName, hostNameToGetChunk, minIndex);
+							requestForChunkThread.start();
+						}
+					}
+					else{
+						
+					}
 				}
 			}
 		} catch (Exception e) {
@@ -63,13 +82,19 @@ public class GetFileThread extends Thread {
 		}
 	}
 	
-	public Hashtable<Integer, String> getChunkToHostMapping(String fileName){
-		Hashtable<Integer, String> chunkToHostMapping = new Hashtable<Integer, String>();
-		Hashtable<String, Integer[]> fileNameChunkReplicationMap = peerStatus.fileNameChunkReplicationMap;
+	public String getHostNameForChunk(String fileName, int minIndex){
+		String hostName = "Nohostnamefound";
+		Map<String, Status> map = listOfOtherPeersStatus;
+		Iterator<Map.Entry<String, Status>> it = map.entrySet().iterator();
+
+		while (it.hasNext()) {
+			Map.Entry<String, Status> entry = it.next();
+			Status otherPeerStatus = entry.getValue();
+			if (otherPeerStatus.containsChunk(fileName + "_chunk_" + minIndex)){
+				hostName = entry.getKey();
+			}
+		}
 		
-		//1 Get which chunk to send first
-		//2 Based on chunk, get which host we can get this chunk from
-		
-		return chunkToHostMapping;
+		return hostName;
 	}
 }
